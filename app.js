61f8b0015a2c71a41576dd2952750a6d4df5d1fb
Hugo_Millot <hugomillot2003@gmail.com>
2024-04-27 20:10:49 +0200

    Revert "change calls to category"
    This reverts commit 0bfeaf0436120adedd16dba489ce86bce7d7bab2.

--- app.js
+++ app.js
@@ -108,8 +108,8 @@ function makeApp() {
       offset = req.query.offset;
     }
     let category = "all";
-    if (req.query.category !== undefined) {
-      category = req.query.category;
+    if (req.params.category !== undefined) {
+      category = req.params.category;
     }
     let sql = `SELECT d.*
         FROM document d
