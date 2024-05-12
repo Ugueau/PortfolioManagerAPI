fe35be0c5292616ed47d2bb1f5367f85326304e7
Hugo_Millot <hugomillot2003@gmail.com>
2024-05-12 13:54:50 +0200

    update accpet cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function makeApp() {
 
   app.use(bodyParser.json());
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:6969"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://86.235.199.160:6969"); // update to match the domain you will make the request from
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
